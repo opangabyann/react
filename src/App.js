7a33f2105b35932ba8a51dc06b75cd27d74d668b
opangabyann <opang4u@gmail.com>
2022-08-11 14:32:24 +0700

    belajar useEffect episode 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import React from "react";
 
-import Header from "./Component/header";
-
-import Page from "./page";
-import Tes from "./Component/module/Tes";
-import {Input , Button} from "./Component/Name"
 
 function App () {
+  let [count,setCount] = React.useState(0)
+  let [text,setText] = React.useState(true)
+  let [message,setMessage] = React.useState(0)
+  let [loading,setLoading] = React.useState(true)
+
+  React.useEffect(()=>{
+    setMessage(message + 1)
+
+    console.log('useEffect berjalan')
+  }, [count,text])
+
+  React.useEffect(()=>{
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000);
+  },[])
+
+  if (loading){
+    return <h1>sabar bentar ....</h1>
+  }
+
   return (
     <React.Fragment>
-      <h1>latihan export import</h1>
-      <Header/>
-      <Page/>
-      <Tes/>
-      <Input />
-      <Button/>
+      <h1>belajar useEffect</h1>
+      <h3>{message === 10 ? "ini sepuluh " : "bukan sepuluh"}</h3>
+      <h1>message : {message}</h1>
+      <h1>count : {count}</h1>
+
+      <button onClick={()=>{
+        setCount(count + 1)
+      }}>
+        tambah
+      </button>
+
+      <button
+      onClick={()=>{
+        setText(!text)
+      }}
+      >
+        ubah
+      </button>
     </React.Fragment>
   );
 }
