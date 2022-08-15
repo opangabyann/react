638c6f3713d4556c27f44b34774e0d45bebc6bb0
opangabyann <opang4u@gmail.com>
2022-08-15 14:20:43 +0700

    instalasi tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App(){
+    return(
+        <React.Fragment>
+            <h1 className="bg-red-500">branch main</h1>
+        </React.Fragment>
+    )
+}
