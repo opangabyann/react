98649d2ac389aeff4cb21e565b76dd2d0f378d90
opangabyann <opang4u@gmail.com>
2022-08-31 13:21:44 +0700

    remed

---
+++ src/component/Card.jsx
@@ -0,0 +1,35 @@
+import Button from "./Button";
+import { showFormattedDate } from "../utils";
+export default function Card({
+  title,
+  body,
+  tahun,
+  createdAt,
+  id,
+  handleDelete,
+}) {
+  return (
+    <div className="border p-5 h-72 w-full shadow-lg rounded-lg space-y-2">
+      <div className="h-1/5">
+        <h5 className="text-xl uppercase">{title}</h5>
+        <p className="text-xs italic">{showFormattedDate(createdAt)}</p>
+      </div>
+      <div className="text-justify h-3/5">
+        <p>{body}</p>
+      </div>
+      <p>tahun terbit: {tahun}</p>
+
+      <div className="grid grid-cols-2 gap-5">
+        <Button
+          onClick={handleDelete}
+          value={id}
+          type="button"
+          title={"Delete"}
+          bg="red"
+        />
+        <Button value={id} title="Arsipkan" bg="blue" />
+        
+      </div>
+    </div>
+  );
+}
